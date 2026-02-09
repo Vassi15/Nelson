b413825129c2e2585841971c4343440d3a352160
JesusVassi <jdvassi@gmail.com>
2026-02-09 12:19:58 +0000

    a
    a

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nelson/', // <--- Asegúrate de que Nelson esté escrito igual que tu repo
+  base: '/', // Vuelve a la raíz normal
 })
